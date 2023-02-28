86d34948413fb18c152500ec7d385ffb622fb53f
RezaASiregar <rezasiregar98@gmail.com>
2023-03-01 03:42:04 +0700

    home task

---
+++ home.js
@@ -0,0 +1,211 @@
+// Aritmatika
+let angka1 = 12 + 11 + 10 + 9 + 8
+console.log(angka1)
+let angka2 = 12 + 11 + 10 - 9 - 8
+console.log(angka2)
+let angka3 = 12 + 11 * 10 / 9 - 8
+console.log(angka3)
+let angka4 = 12 - 11 * 10 ** 9 / 8
+console.log(angka4)
+let angka5 = 12 ** 11 * 10 / 9 * 8
+console.log(angka5)
+let angka6 = 12 + 11 - 10 ** 9 * 8
+console.log(angka6)
+let angka7 = 12 * 11 / 10 ** 9 + 8
+console.log(angka7)
+let angka8 = 12 % 11 * 10 + 9 ** 8
+console.log(angka8)
+let angka9 = 12 - 11 - 10 - 9 ** 8
+console.log(angka9)
+let angka10 = 12 * 11 * 10 / 9 + 8
+console.log(angka10)
+let angka11 = 12 - 11 ** 10 % 9 - 8
+console.log(angka11)
+let angka12 = 12 % 11 + 10 - 9 - 8
+console.log(angka12)
+let angka13 = 12 ** 11 + 10 + 9 + 8
+console.log(angka13)
+let angka14 = 12 * 11 ** 10 / 9 - 8
+console.log(angka14)
+let angka15 = 12 - 11 + 10 - 9 + 8
+console.log(angka15)
+let angka16 = 12 ** 11 * 10 ** 9 * 8
+console.log(angka16)
+let angka17 = 12 / 11 / 10 / 9 / 8
+console.log(angka17)
+let angka18 = 12 - 11 - 10 - 9 - 8
+console.log(angka18)
+let angka19 = 12 * 11 * 10 * 9 * 8
+console.log(angka19)
+let angka20 = 12 / 11 * 10 ** 9 - 8
+console.log(angka20)
+
+//Perbandingan
+//*lebih dari (>)
+let perbandingan1 = 10 > 5 
+console.log(perbandingan1)
+let perbandingan2 = 10 > 10
+console.log(perbandingan2)
+let perbandingan3 = 10 > 15 
+console.log(perbandingan3)
+let perbandingan4 = 10 > 1
+console.log(perbandingan4)
+let perbandingan5 = 10 > 2 
+console.log(perbandingan5)
+let perbandingan6 = 10 > 3
+console.log(perbandingan6)
+let perbandingan7 = 12 > 20 
+console.log(perbandingan7)
+let perbandingan8 = 13 > 40
+console.log(perbandingan8)
+let perbandingan9 = 14 > 60 
+console.log(perbandingan9)
+let perbandingan10 = 15 > 70
+console.log(perbandingan10)
+//*kecil dari (<)
+let perbandingan11 = 10 < 5 
+console.log(perbandingan11)
+let perbandingan12 = 10 < 10
+console.log(perbandingan12)
+let perbandingan13 = 10 < 15 
+console.log(perbandingan13)
+let perbandingan14 = 10 < 1
+console.log(perbandingan14)
+let perbandingan15 = 10 < 2 
+console.log(perbandingan15)
+let perbandingan16 = 10 < 3
+console.log(perbandingan16)
+let perbandingan17 = 12 < 20 
+console.log(perbandingan17)
+let perbandingan18 = 13 < 40
+console.log(perbandingan18)
+let perbandingan19 = 14 < 60 
+console.log(perbandingan19)
+let perbandingan20 = 15 < 70
+console.log(perbandingan20)
+//*lebih dari sama dengan (>=)
+let perbandingan21 = 10 >= 5 
+console.log(perbandingan21)
+let perbandingan22 = 10 >= 10
+console.log(perbandingan22)
+let perbandingan23 = 10 >= 15 
+console.log(perbandingan23)
+let perbandingan24 = 15 >= 10
+console.log(perbandingan24)
+let perbandingan25 = 15 >= 15 
+console.log(perbandingan25)
+let perbandingan26 = 15 >= 20
+console.log(perbandingan26)
+let perbandingan27 = 12 >= 6 
+console.log(perbandingan27)
+let perbandingan28 = 12 >= 12
+console.log(perbandingan28)
+let perbandingan29 = 12 >= 18 
+console.log(perbandingan29)
+let perbandingan30 = 15 >= 70
+console.log(perbandingan30)
+//*kurang dari sama dengan (<=)
+let perbandingan31 = 10 <= 5 
+console.log(perbandingan31)
+let perbandingan32 = 10 <= 10
+console.log(perbandingan32)
+let perbandingan33 = 10 <= 15 
+console.log(perbandingan33)
+let perbandingan34 = 15 <= 10
+console.log(perbandingan34)
+let perbandingan35 = 15 <= 15 
+console.log(perbandingan35)
+let perbandingan36 = 15 <= 20
+console.log(perbandingan36)
+let perbandingan37 = 12 <= 6 
+console.log(perbandingan37)
+let perbandingan38 = 12 <= 12
+console.log(perbandingan38)
+let perbandingan39 = 12 <= 18 
+console.log(perbandingan39)
+let perbandingan40 = 15 <= 70
+console.log(perbandingan40)
+//*sama dengan (==)
+let perbandingan41 = 10 == 5 
+console.log(perbandingan41)
+let perbandingan42 = 10 == 10
+console.log(perbandingan42)
+let perbandingan43 = 10 == 15 
+console.log(perbandingan43)
+let perbandingan44 = 15 == 10
+console.log(perbandingan44)
+let perbandingan45 = 15 == 15 
+console.log(perbandingan45)
+let perbandingan46 = 15 == 20
+console.log(perbandingan46)
+let perbandingan47 = 12 == 6 
+console.log(perbandingan47)
+let perbandingan48 = 12 == 12
+console.log(perbandingan48)
+let perbandingan49 = 12 == 18 
+console.log(perbandingan49)
+let perbandingan50 = 15 == 70
+console.log(perbandingan50)
+//*sama dengan dan sama tipe (===)
+let perbandingan51 = 10 === 5 
+console.log(perbandingan51)
+let perbandingan52 = 10 === 10
+console.log(perbandingan52)
+let perbandingan53 = 10 === 15 
+console.log(perbandingan53)
+let perbandingan54 = 15 === 10
+console.log(perbandingan54)
+let perbandingan55 = 15 === 15 
+console.log(perbandingan55)
+let perbandingan56 = 15 === 20
+console.log(perbandingan56)
+let perbandingan57 = 12 === 6 
+console.log(perbandingan57)
+let perbandingan58 = 12 === 12
+console.log(perbandingan58)
+let perbandingan59 = 12 === 18 
+console.log(perbandingan59)
+let perbandingan60 = 15 === 70
+console.log(perbandingan60)
+//*tidak sama dengan (!=)
+let perbandingan61 = 10 != 5 
+console.log(perbandingan61)
+let perbandingan62 = 10 != 10
+console.log(perbandingan62)
+let perbandingan63 = 10 != 15 
+console.log(perbandingan63)
+let perbandingan64 = 15 != 10
+console.log(perbandingan64)
+let perbandingan65 = 15 != 15 
+console.log(perbandingan65)
+let perbandingan66 = 15 != 20
+console.log(perbandingan66)
+let perbandingan67 = 12 != 6 
+console.log(perbandingan67)
+let perbandingan68 = 12 != 12
+console.log(perbandingan68)
+let perbandingan69 = 12 != 18 
+console.log(perbandingan69)
+let perbandingan70 = 15 != 70
+console.log(perbandingan70)
+//*tidak sama dengan atau tidak sama tipe (!==)
+let perbandingan71 = 10 !== 5 
+console.log(perbandingan71)
+let perbandingan72 = 10 !== 10
+console.log(perbandingan72)
+let perbandingan73 = 10 !== 15 
+console.log(perbandingan73)
+let perbandingan74 = 15 !== 10
+console.log(perbandingan74)
+let perbandingan75 = 15 !== 15 
+console.log(perbandingan75)
+let perbandingan76 = 15 !== 20
+console.log(perbandingan76)
+let perbandingan77 = 12 !== 6 
+console.log(perbandingan77)
+let perbandingan78 = 12 !== 12
+console.log(perbandingan78)
+let perbandingan79 = 12 !== 18 
+console.log(perbandingan79)
+let perbandingan80 = 15 !== 70
+console.log(perbandingan80)
